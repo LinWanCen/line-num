7365f69dd78adae87ec5577933ec4a794df4f522
GitHub Action <action@github.com>
2022-11-23 15:29:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/linwancen/findauthor/services/MyProjectService.kt
+++ src/main/kotlin/com/github/linwancen/findauthor/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.linwancen.findauthor.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.linwancen.findauthor.MyBundle
 
 class MyProjectService(project: Project) {
 
